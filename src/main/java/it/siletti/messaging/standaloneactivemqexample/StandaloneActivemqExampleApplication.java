9005e64177766018df0eeda1f2818a4eef09be2c
siletti <alberto.siletti@gmail.com>
2019-10-03 17:31:51 +0200

    Initial commit

---
+++ src/main/java/it/siletti/messaging/standaloneactivemqexample/StandaloneActivemqExampleApplication.java
@@ -0,0 +1,13 @@
+package it.siletti.messaging.standaloneactivemqexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandaloneActivemqExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandaloneActivemqExampleApplication.class, args);
+	}
+
+}
